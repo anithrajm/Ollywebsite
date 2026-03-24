b1aa25250117351a7f6db3c9006a3542906a8eb0
anithrajm <anithraja412@gmail.com>
2026-03-24 10:47:36 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Home from "./component/Home";
+import "./App.css";
+
+
+
+export default function app(){
+
+  return(
+    <div>
+      <Home />
+    </div>
+  )
+}
+
+
---
+++ src/component/Home.jsx
@@ -0,0 +1,89 @@
+import { Search, ShoppingCart } from 'lucide-react'
+
+
+import sleepImg from "../assets/1.png";
+import immunityImg from "../assets/2.png";
+import beatyImg from "../assets/3.png";
+import moodImg from "../assets/4.png";
+import React from 'react'
+
+
+
+export default function Home() {
+
+const categories=[
+
+    {
+        title:"SWEET SLEEP",
+        img: sleepImg,
+    },
+
+    {
+        title:"IMMUNITY DEFENSE",
+        img: immunityImg,
+    },
+    {
+        title:"BEAUTY FROM WITHIN",
+        img: beatyImg,
+
+    },
+
+    {
+        title:"MOOD SUPPORT",
+        img: moodImg,
+    }
+]
+
+
+
+  return (
+    <div className="container">
+
+    <div className="topbanner">
+
+        Free Shipping on Orders Over $59 | SHOP NOW
+
+    </div>
+
+<div className="navbar">
+
+    <h1 className="logo">OLLY.</h1>
+
+    <div className="navlinks">
+        <p>SHOP</p>
+        <p>INSIDE THE PRODUCT</p>
+        <p>ABOUT</p>
+        <p>OUR MISSION</p>
+    </div>
+
+
+    <div className="navicons">
+        <Search  className="search"size={25} />
+        <p className="login">LOGIN</p>
+
+        <button className="shopbtn">SHOP NOW</button>
+
+        <ShoppingCart className="shopingcart" size={25} />
+    </div>
+</div>
+
+<h2 className="titel">Shop by Category</h2>
+
+<div className="cardcontainer">
+    {
+        categories.map((item,index)=>(
+            <div className="card" key={index}>
+                <img src={item.img} alt={item.title}/>
+                <p>{item.title}</p>
+                
+            </div>
+        ))
+    }
+</div>
+
+
+<button className='supportbtn'> ? Support</button>
+
+    </div>
+  );
+}
